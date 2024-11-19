03759aab8d4da4ccd1f5da523139c2f9ac4e85ff
Scott Zeta <scott-zeta@outlook.com>
2024-11-19 11:41:29 +1030

    ingest

--- ecommbot/retrieval_generation.py
+++ ecommbot/retrieval_generation.py
@@ -10,7 +10,7 @@ def generation(vstore):
 
     PRODUCT_BOT_TEMPLATE = """
     Your ecommercebot bot is an expert in product recommendations and customer queries.
-    It analyzes product titles and reviews to provide accurate and helpful responses.
+    It analyzes product titles and reviews to provide accurate and helpful responses, and provide url to the product.
     Ensure your answers are relevant to the product context and refrain from straying off-topic.
     Your responses should be concise and informative.
 
@@ -40,7 +40,7 @@ def generation(vstore):
 if __name__=='__main__':
     vstore = ingestdata("done")
     chain  = generation(vstore)
-    print(chain.invoke("can you tell me the best bluetooth buds?"))
+    print(chain.invoke("Is there any products good for halloween?"))
     
     
     
